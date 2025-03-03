ef3ce227624b39cae6934a4e52a779006da381b0
DaoVietLong <daovietlong10@gmail.com>
2025-03-03 17:22:40 +0100

    initialize 3 child classes for 3 kinds of movie of abstract class Price

--- refactoring_dirige_partie_1_corrige/src/main/java/fr/r6a06/ChildrenPrice.java
+++ refactoring_dirige_partie_1_corrige/src/main/java/fr/r6a06/ChildrenPrice.java
@@ -1,4 +1,16 @@
 package fr.r6a06;
 
-public class ChildrenPrice {
+class ChildrensPrice extends Price {
+    @Override
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+    }
 }
--- refactoring_dirige_partie_1_corrige/src/main/java/fr/r6a06/NewReleasePrice.java
+++ refactoring_dirige_partie_1_corrige/src/main/java/fr/r6a06/NewReleasePrice.java
@@ -1,4 +1,21 @@
 package fr.r6a06;
 
-public class NewReleasePrice {
+class NewReleasePrice extends Price {
+    @Override
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        if (daysRented > 1) {
+            return 2;
+        }
+        return 1;
+    }
 }
