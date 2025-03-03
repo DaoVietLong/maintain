86bde19382e6b00005ea17b2bacccb0c091740ed
DaoVietLong <daovietlong10@gmail.com>
2025-03-03 16:58:17 +0100

    refactor: move method getCharge from class Rental to class Movie

--- refactoring_dirige_partie_1_corrige/src/main/java/fr/r6a06/Movie.java
+++ refactoring_dirige_partie_1_corrige/src/main/java/fr/r6a06/Movie.java
@@ -21,4 +21,30 @@ public class Movie {
     public String getTitle (){
         return _title;
     };
+
+    public double getCharge(int dayRented) {
+        double result = 0.0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (dayRented > 2) {
+                    result += (dayRented - 2) * 1.5;
+                }
+                else{
+                    break;
+                }
+            case NEW_RELEASE:
+                result += dayRented * 3;
+                break;
+            case CHILDRENS:
+                result += 1.5;
+                if (dayRented > 3) {
+                    result += (dayRented - 3) * 1.5;
+                }
+                else {
+                    break;
+                }
+        }
+        return result;
+    }
 }
